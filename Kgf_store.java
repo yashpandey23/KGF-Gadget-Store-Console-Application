ab76e5612f2fb903f4b0039bcceec90ff104db51
Yash pandey <95755491+yashpandey23@users.noreply.github.com>
2023-03-09 13:06:02 +0530

    Add files via upload

---
+++ Kgf_store.java
@@ -0,0 +1,194 @@
+import java.util.Scanner;
+public class Kgf_store {
+    public static void main(String[]args)
+    {
+Scanner scan = new Scanner(System.in);
+System.out.println( );
+System.out.println("welcome to kgf gadget store");
+System.out.println("===================================================");
+System.out.println("                                           ");
+System.out.println("ENTER 1 FOR MOBILES PHONE");
+System.out.println("ENTER 2 FOR LAPTOPS");
+System.out.println("ENTER 3 FOR SMARTWATCH");
+System.out.println("ENTER 4 FOR TV");
+System.out.println("ENTER 5 FOR AC");
+System.out.println("===================================================");
+System.out.println("                                                       ");
+System.out.println("Enter your option you want to choice ");
+int choice = scan.nextInt();
+
+if(choice ==1){
+    System.out.println("YOU HAVE ENTERED IN TO THE WORLD OF PHONE");
+    System.out.println("===================================================");
+System.out.println("                                           ");
+System.out.println("ENTER 1 FOR IPHONE");
+System.out.println("ENTER 2 FOR ONEPLUS");
+System.out.println("ENTER 3 FOR SAMSUNG");
+System.out.println("ENTER 4 FOR NOKIA");
+System.out.println("ENTER 5 FOR MI");
+System.out.println("===================================================");
+System.out.println("                                                       ");
+System.out.println("Enter your option you want to choice ");
+int choice1 = scan.nextInt();
+if(choice1 ==1){
+    System.out.println("Availability : in stock");
+    System.out.println("price is : - 145000");
+}else if(choice1 ==2){
+    System.out.println("Availability : in stock");
+    System.out.println("price is : - 45000");
+}else if(choice1 ==3){
+    System.out.println("Availability : out stock");
+    System.out.println("price is : - 25000");
+}else if(choice1 ==4){
+    System.out.println("Availability : in stock");
+    System.out.println("price is : - 15000");
+}else if(choice1 ==5){
+    System.out.println("Availability : in stock");
+    System.out.println("price is : - 10000");
+}else{
+    System.out.println("plese enter a vaild input");
+}
+}
+
+
+else if(choice ==2){
+    System.out.println("YOU HAVE ENTERED IN TO THE WORLD OF LAPTOPS");
+    System.out.println("===================================================");
+System.out.println("                                           ");
+System.out.println("ENTER 1 FOR MAC");
+System.out.println("ENTER 2 FOR HP");
+System.out.println("ENTER 3 FOR DELL");
+System.out.println("ENTER 4 FOR ACES");
+System.out.println("ENTER 5 FOR MI");
+System.out.println("===================================================");
+System.out.println("                                                       ");
+System.out.println("Enter your option you want to choice ");
+int choice2 = scan.nextInt();
+if(choice2 ==1){
+    System.out.println("Availability : in stock");
+    System.out.println("price is :- 245000");
+}else if(choice2 ==2){
+    System.out.println("Availability : in stock");
+    System.out.println("price is :- 145000");
+}else if(choice2 ==3){
+    System.out.println("Availability : out of  stock");
+    System.out.println("price is :- 95000");
+}else if(choice2 ==4){
+    System.out.println("Availability : out of stock");
+    System.out.println("price is :- 85000");
+}else if(choice2 ==5){
+    System.out.println("Availability : in stock");
+    System.out.println("price is :- 50000");
+}else{
+    System.out.println("please enter a vaild input");
+}
+}
+
+
+else if(choice ==3){
+    System.out.println("YOU HAVE ENTERED IN TO THE WORLD OF SMART WATHC");
+    System.out.println("===================================================");
+System.out.println("                                           ");
+System.out.println("ENTER 1 FOR IWATCH");
+System.out.println("ENTER 2 FOR BOLT");
+System.out.println("ENTER 3 FOR NOISE");
+System.out.println("ENTER 4 FOR BOAT");
+System.out.println("ENTER 5 FOR FOSSIL");
+System.out.println("===================================================");
+System.out.println("                                                       ");
+System.out.println("Enter your option you want to choice ");
+int choice3 = scan.nextInt();
+if(choice3 ==1){
+    System.out.println("Availability : in stock");
+    System.out.println("price is :- 55000");
+}else if(choice3 ==2){
+    System.out.println("Availability : in stock");
+    System.out.println("price is :- 5000");
+}else if(choice3 ==3){
+    System.out.println("Availability : out of  stock");
+    System.out.println("price is :- 9000");
+}else if(choice3 ==4){
+    System.out.println("Availability : out of stock");
+    System.out.println("price is :- 6000");
+}else if(choice3 ==5){
+    System.out.println("Availability : out of stock");
+    System.out.println("price is :- 3000");
+}
+else{
+    System.out.println("plese enter a vaild input");
+}
+}
+
+
+else if(choice ==4){
+    System.out.println("YOU HAVE ENTERED IN TO THE WORLD OF TV");
+    System.out.println("===================================================");
+System.out.println("                                           ");
+System.out.println("ENTER 1 FOR LG");
+System.out.println("ENTER 2 FOR SONY");
+System.out.println("ENTER 3 FOR SAMSUNG");
+System.out.println("ENTER 4 FOR ONEPLUS");
+System.out.println("ENTER 5 FOR MI");
+System.out.println("===================================================");
+System.out.println("                                                       ");
+System.out.println("Enter your option you want to choice ");
+int choice4 = scan.nextInt();
+if(choice4 ==1){
+    System.out.println("Availability : in stock");
+    System.out.println("price is :- 105000");
+}else if(choice4 ==2){
+    System.out.println("Availability : in stock");
+    System.out.println("price is :- 45000");
+}else if(choice4 ==3){
+    System.out.println("Availability : out of  stock");
+    System.out.println("price is :- 50000");
+}else if(choice4 ==4){
+    System.out.println("Availability : out of stock");
+    System.out.println("price is :- 85000");
+}else if(choice4 ==5){
+    System.out.println("Availability : in stock");
+    System.out.println("price is :- 15000");
+}else{
+    System.out.println("plese enter a vaild input");
+}
+}
+
+
+else if(choice ==5){
+    System.out.println("YOU HAVE ENTERED IN TO THE WORLD OF AC");
+    System.out.println("===================================================");
+System.out.println("                                           ");
+System.out.println("ENTER 1 FOR BLUESTARE");
+System.out.println("ENTER 2 FOR WHIRLPOOL");
+System.out.println("ENTER 3 FOR VOLTAGE");
+System.out.println("ENTER 4 FOR SMANSUNG");
+System.out.println("ENTER 5 FOR ONIPA");
+System.out.println("===================================================");
+System.out.println("                                                       ");
+System.out.println("Enter your option you want to choice ");
+int choice5 = scan.nextInt();
+if(choice5 ==1){
+    System.out.println("Availability : out of stock");
+    System.out.println("price is :- 245000");
+}else if(choice5 ==2){
+    System.out.println("Availability : in stock");
+    System.out.println("price is :- 145000");
+}else if(choice5 ==3){
+    System.out.println("Availability : out of  stock");
+    System.out.println("price is :- 95000");
+}else if(choice5 ==4){
+    System.out.println("Availability : out of stock");
+    System.out.println("price is :- 85000");
+}else if(choice5 ==5){
+    System.out.println("Availability : in stock");
+    System.out.println("price is :- 50000");
+}else{
+    System.out.println("please enter a vaild input");
+}
+}
+else {
+    System.out.println("please enter a vaild input");
+}
+    }
+    
+}
